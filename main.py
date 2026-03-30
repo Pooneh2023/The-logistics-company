a1131630653c1df2d48ef4971928154f0405357e
Pooneh2023 <pooneh.nezamabadi1990@gmail.com>
2026-03-30 15:24:10 +0200

    Modified README, main.py and pyproject.toml

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from the-logistics-company!")
+from src.data.clean_orders import clean_orders
+from src.data.merge_data import merge_data
 
+import pandas as pd
+
+def run_pipeline():
+    orders = pd.read_excel("data/raw/Muesli Project raw data.xlsx")
+
+    clean = clean_orders(orders)
+
+    clean.to_excel("data/cleaned/clean_orders.xlsx", index=False)
+
+    final = merge_data()
+
+    final.to_excel("data/final/final_data.xlsx", index=False)
 
 if __name__ == "__main__":
-    main()
+    run_pipeline()
